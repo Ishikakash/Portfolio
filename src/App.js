92bfea3a410e5a15bf6021aa3e87b3eaee294e33
Ishika Kashyap <ishikakashyap2412@gmail.com>
2023-02-08 19:02:54 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
 import Hero from "./components/Hero";
+import MySkills from "./components/MySkills";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+import Work from "./components/Work";
 import AboutMe from "./components/AboutMe";
 function App() {
   return (
     <>
-      <Hero />
-      <AboutMe />
+      <Hero/>
+      <AboutMe/>
+      <MySkills/>
+      <Skills/>
+      <Work/>
+      <Contact/>
     </>
   );
 }
